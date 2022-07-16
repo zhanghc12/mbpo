0ce846cf0fa159ce993acd480bb545f035b5e280
zhanghc <zhanghc12@126.com>
2022-07-16 17:40:07 +0800

    1

--- mbpo/models/bnn.py
+++ mbpo/models/bnn.py
@@ -357,7 +357,7 @@ class BNN:
             for batch_num in range(int(np.ceil(idxs.shape[-1] / batch_size))):
                 #batch_idxs = idxs[:, batch_num * batch_size:(batch_num + 1) * batch_size]
                 # modified by zhc
-                powererd_priority = np.squeeze(np.power(priority, 1)) + 1e-5
+                powererd_priority = np.abs(np.squeeze(np.power(priority, 1)) + 6) + 1e-5
                 batch_idxs = np.random.choice(np.arange(inputs.shape[0]), batch_size * 7, p=powererd_priority/(powererd_priority.sum()))
                 batch_idxs = batch_idxs.reshape([7, 256])
                 self.sess.run(
